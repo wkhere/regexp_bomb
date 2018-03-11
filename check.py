31903c408724f8a3063a4a2bf2e9354188691c99
Wojciech Kaczmarek <wojtekk@kofeina.net>
2018-03-11 19:32:14 +0100

    py: an() returns compiled regex as for other langs

--- check.py
+++ check.py
@@ -2,9 +2,9 @@
 
 import re, sys
 
-def an(n): return ('a?'*n+'a'*n, 'a'*n)
+def an(n): return (re.compile('a?'*n+'a'*n), 'a'*n)
 
 n = int(sys.argv[1])
-p, s = an(n)
-idxs = re.match(p,s).span()
+r, s = an(n)
+idxs = r.match(s).span()
 print 'n:', n, 'match:', '[%s]' % ' '.join(map(str, idxs))
